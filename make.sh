781c45fb533204e5e4b2f55495f038adbd3f6eff
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-12-25 07:07:43 +0000

    update: Updated the code to check the functionality

--- make.sh
+++ make.sh
@@ -1,9 +1,9 @@
-MODULE_NAME = bubble_sort 
-OUTPUT_JS = src/wasm/bubble_sort.js
-OUTPUT_WASM = src/wasm/bubble_sort.wasm
+MODULE_NAME=bubble_sort
+OUTPUT_JS=src/wasm/bubble_sort.js
+OUTPUT_WASM=src/wasm/bubble_sort.wasm
 
 emcc cpp/${MODULE_NAME}.cpp \
     -o ${OUTPUT_JS} \
-    -s EXPORT_ES6 = 1 \
-    -s 'EXPORT_NAME = "$MODULE_NAME"' \
-    -S 'ENVIRONMENT = "web"'
+    -s EXPORT_ES6=1 \
+    -s EXPORT_NAME="$MODULE_NAME" \
+    -s ENVIRONMENT="web"
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
+import { useState } from 'react';
 import './App.css';
 
+// Import the WebAssembly and JS files
+import WebAssemblyBinary from './wasm/bubble_sort.wasm';
+
 function App() {
+  const [numbers, setNumbers] = useState({
+    a: 0,
+    b: 0,
+  });
+  const [result, setResult] = useState(0);
+
+  // Load WebAssembly
+  const loadWasm = async () => {
+    const wasmModule = await WebAssembly.instantiateStreaming(fetch(WebAssemblyBinary), {
+      env: {
+        // You can add imports if needed
+      }
+    });
+    return wasmModule.instance.exports;
+  };
+
+  const handleClick = async () => {
+    const wasmCore = await loadWasm();
+
+    // Ensure numbers are converted to integers
+    const res = wasmCore.adder(Number(numbers.a), Number(numbers.b));
+    setResult(res);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        onChange={(e) => setNumbers({ ...numbers, a: e.target.value })}
+      />
+      <input
+        type="text"
+        onChange={(e) => setNumbers({ ...numbers, b: e.target.value })}
+      />
+      <button onClick={handleClick}>Add</button>
+      <div className="bg-red-300">{result}</div>
     </div>
   );
 }
